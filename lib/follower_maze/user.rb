de90040d18d61776fa54700b1814bcc2f2448fb0
Willa Wang <willa@neo.com>
2015-01-20 08:47:21 -0500

    Adding follower handling to user.

--- lib/follower_maze/user.rb
+++ lib/follower_maze/user.rb
@@ -1,9 +1,24 @@
 module FollowerMaze
   class User
-    attr_reader :id
+    attr_reader :id, :conn, :followers
 
-    def initialize(id)
+    def initialize(id, conn=nil)
       @id = id
+      @conn = conn
+      @followers = []
+    end
+
+    def add_follower(follower_id)
+
+      @followers << follower_id
+    end
+
+    def remove_follower(follower_id)
+      @followers.delete(follower_id)
+    end
+
+    def notify(message)
+      @conn.puts message if @conn
     end
   end
 end
--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -1,11 +1,30 @@
 require "spec_helper"
+
 describe FollowerMaze::User do
   let(:id) { 5 }
-  subject { described_class.new(id) }
+  let(:conn) { {} }
+  subject { described_class.new(id, conn) }
 
   describe "#initialize" do
     it "creates a new user" do
       expect(subject.id).to eq(5)
+      expect(subject.conn).to eq({})
+    end
+  end
+
+  describe "#add_follower" do
+    it "adds the follower's id to the user's followers array" do
+      subject.add_follower("new user")
+      expect(subject.followers).to eq(["new user"])
+    end
+  end
+
+  describe "#remove_follower" do
+    it "removes the follower's id from the user's followers array" do
+      subject.add_follower("follower")
+      subject.remove_follower("follower")
+
+      expect(subject.followers).to eq([])
     end
   end
 end
