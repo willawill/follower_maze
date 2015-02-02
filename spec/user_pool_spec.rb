edb73db36da9c426d8f3e324b4b81bf1c78fc5d9
Willa Wang <willa@neo.com>
2015-02-01 19:08:46 -0500

    Adding user pool to handle global users.

--- spec/user_pool_spec.rb
+++ spec/user_pool_spec.rb
@@ -3,36 +3,37 @@ require "spec_helper"
 module FollowerMaze
   describe UserPool do
     subject { UserPool }
-    let(:user_double) { instance_double(User, id: "id") }
+    let(:user) { User.new("id", nil) }
+    let(:connection) { double(puts: "message") }
 
     describe ".all_users" do
-      it "returns all connected users" do
+      it "returns all users" do
         expect(subject.all_users).to eq({})
       end
     end
 
     describe ".add_or_update_user" do
-      it "adds new id : user_instance pair to UserPool" do
-        subject.add_or_update_user(user_double)
-        expect(subject.all_users).to eq({"id" => user_double})
+      it "updates the user with current connection situation" do
+        allow(subject).to receive(:find_or_create_user).and_return(user)
+        subject.add_or_update_user("id", connection)
+
+        expect(user.conn).to eq(connection)
       end
     end
 
     describe ".remove_user" do
       it "removes the id: user_instance pair from UserPool" do
-        subject.add_user(user_double)
+        subject.add_or_update_user("id", nil)
 
-        subject.remove_user(user_double.id)
+        subject.remove_user(user.id)
         expect(subject.all_users).to eq({})
       end
     end
 
-    describe ".find_user" do
+    describe ".find_or_create_user" do
       it "finds user_instance from UserPool with user id" do
-        subject.add_user(user_double)
-
-        user = subject.find_user("id")
-        expect(user).to eq(user_double)
+        user = subject.find_or_create_user("id")
+        expect(user).to eq(user)
       end
     end
   end
